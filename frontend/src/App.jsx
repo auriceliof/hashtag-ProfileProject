b3131457edda1e9dcfba346c658a6f3451bcdc9e
auriceliof <auricelio.suporte@hotmail.com>
2025-12-23 16:38:09 -0300

    add  Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { Header } from "./components/header/Header";
 import "./global.css";
+import { Header } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
 
 export function App() {
   return (
     <>
       <Header />
       <div className="app"></div>
+      <Footer />
     </>
   );
 }

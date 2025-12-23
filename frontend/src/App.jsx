ff8098a8590650eeeda7269d20ed3c0b2e92ba45
auriceliof <auricelio.suporte@hotmail.com>
2025-12-23 15:17:10 -0300

    add Header and Styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Header } from "./components/header/Header";
 
 export function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }

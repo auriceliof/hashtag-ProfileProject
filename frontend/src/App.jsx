d1ee5067d9f7b78d6061c0a29ed3764922054c95
auriceliof <auricelio.suporte@hotmail.com>
2025-12-23 16:04:52 -0300

    add background image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "./components/header/Header";
+import "./global.css";
 
 export function App() {
   return (
     <>
       <Header />
+      <div className="app"></div>
     </>
   );
 }

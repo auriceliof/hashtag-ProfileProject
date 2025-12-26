7767311f931a57cc35a19d8645e9c602921db8c1
auriceliof <auricelio.suporte@hotmail.com>
2025-12-25 23:03:58 -0300

    Add Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import "./global.css";
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
+import { Profile } from "./components/profile/Profile";
 
 export function App() {
   return (
     <>
       <Header />
-      <div className="app"></div>
+      <div className="app">
+        <Profile 
+          avatar="https://randomuser.me/api/portraits/women/44.jpg"
+          name="Anna Caroline Dias"
+          bio="Desenvolvedora FullStack"
+          phone="85985741276"
+          email="ana.carol.dias@email.com"
+          gitHubUrl="https://github.com/"
+          linkedinUrl="https://www.linkedin.com/login/pt"
+          instagramUrl="https://www.instagram.com/"
+        />
+      </div>
       <Footer />
     </>
   );
---
+++ frontend/src/components/profile/Profile.jsx
@@ -0,0 +1,27 @@
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+
+export function Profile(props) {
+  return (
+    <div>
+      <img src={props.avatar} alt={props.name} />
+      <h2>{props.name}</h2>
+      <div>{props.bio}</div>
+      <div>{props.phone}</div>
+      <div>{props.email}</div>
+      <div>
+        <a href={props.gitHubUrl} target="_blank" rel="noreferer">
+            <FaGithub />
+            Github
+        </a>
+        <a href={props.linkedinUrl} target="_blank" rel="noreferer">
+            <FaLinkedin />
+            Linkedin
+        </a>
+        <a href={props.instagramUrl} target="_blank" rel="noreferer">
+            <FaInstagram />
+            Instagram
+        </a>
+      </div>
+    </div>
+  )
+}

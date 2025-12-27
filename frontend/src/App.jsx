dcd1ba4a90290c3f243db26e77bfe33ef0e4f48e
auriceliof <auricelio.suporte@hotmail.com>
2025-12-26 22:37:23 -0300

    Add Profile styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,64 @@ export function App() {
           linkedinUrl="https://www.linkedin.com/login/pt"
           instagramUrl="https://www.instagram.com/"
         />
+
+        <Profile 
+          avatar="https://randomuser.me/api/portraits/men/32.jpg"
+          name="João Paulo Souza"
+          bio="Engenheiro Backend"
+          phone="85925347513"
+          email="joao.paulo.souza@email.com"
+          gitHubUrl="https://github.com/"
+          linkedinUrl="https://www.linkedin.com/login/pt"
+          instagramUrl="https://www.instagram.com/"
+          theme="male"
+        />
+
+        <Profile 
+          avatar="https://randomuser.me/api/portraits/women/55.jpg"
+          name="Mariana Duarte"
+          bio="Designer UI/UX"
+          phone="85981741398"
+          email="mariana.duarte@email.com"
+          gitHubUrl="https://github.com/"
+          linkedinUrl="https://www.linkedin.com/login/pt"
+          instagramUrl="https://www.instagram.com/"
+        />
+
+        <Profile 
+          avatar="https://randomuser.me/api/portraits/men/65.jpg"
+          name="Diego Gonçalves"
+          bio="Desenvolvedor Frontend"
+          phone="85991451980"
+          email="diego.goncalves@email.com"
+          gitHubUrl="https://github.com/"
+          linkedinUrl="https://www.linkedin.com/login/pt"
+          instagramUrl="https://www.instagram.com/"
+          theme="male"
+        />
+
+        <Profile 
+          avatar="https://randomuser.me/api/portraits/women/75.jpg"
+          name="Fabiana Oliveira"
+          bio="Designer UI/UX"
+          phone="8592134578"
+          email="fabiana.oliveira@email.com"
+          gitHubUrl="https://github.com/"
+          linkedinUrl="https://www.linkedin.com/login/pt"
+          instagramUrl="https://www.instagram.com/"
+        />
+
+        <Profile 
+          avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          name="Rogerio filho"
+          bio="Desenvolvedor Frontend"
+          phone="8594311028"
+          email="rogerio.filho@email.com"
+          gitHubUrl="https://github.com/"
+          linkedinUrl="https://www.linkedin.com/login/pt"
+          instagramUrl="https://www.instagram.com/"
+          theme="male"
+        />
       </div>
       <Footer />
     </>
--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -1,25 +1,39 @@
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import styles from "./profile.module.css";
 
 export function Profile(props) {
   return (
-    <div>
-      <img src={props.avatar} alt={props.name} />
-      <h2>{props.name}</h2>
-      <div>{props.bio}</div>
-      <div>{props.phone}</div>
-      <div>{props.email}</div>
-      <div>
-        <a href={props.gitHubUrl} target="_blank" rel="noreferer">
-            <FaGithub />
+    <div className={styles.container}>
+      <img src={props.avatar} alt={props.name} className={styles.avatar}/>
+      <h2 className={styles.name}>{props.name}</h2>
+      <div className={styles.info}>{props.bio}</div>
+      <div className={styles.info}>{props.phone}</div>
+      <div className={styles.info}>{props.email}</div>
+      <div className={styles.links}>
+        <a
+          href={props.gitHubUrl} 
+          target="_blank" 
+          rel="noreferer" 
+          className={props.theme === "male" ? styles.linkButtonMale : styles.linkButton}
+        >
+          <FaGithub className={styles.icon} />
             Github
         </a>
-        <a href={props.linkedinUrl} target="_blank" rel="noreferer">
-            <FaLinkedin />
-            Linkedin
+        <a href={props.linkedinUrl} 
+          target="_blank" 
+          rel="noreferer" 
+          className={props.theme === "male" ? styles.linkButtonMale : styles.linkButton}
+        >
+          <FaLinkedin className={styles.icon} />
+          Linkedin
         </a>
-        <a href={props.instagramUrl} target="_blank" rel="noreferer">
-            <FaInstagram />
-            Instagram
+        <a href={props.instagramUrl} 
+          target="_blank" 
+          rel="noreferer" 
+          className={props.theme === "male" ? styles.linkButtonMale : styles.linkButton}
+        >
+          <FaInstagram className={styles.icon} />
+          Instagram
         </a>
       </div>
     </div>
